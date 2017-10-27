7ede9be71d59e6a838282c4cb8a92b44e8420eaf
Logan <larnold3@nd.edu>
2017-10-27 10:54:38 -0400

    Adding initial pseduo-code

---
+++ Exercise09.R
@@ -0,0 +1,16 @@
+#### Pseudo-code ####
+
+#1. Load Data
+
+#2 Custom function(params, observations)
+# unpack parameters
+# Assign parameters to expected values
+# dnorm(x,mean,sd)
+
+#3 Assess max likelihood of model parameters with optim()
+# Create a vector with initial guesses
+# Minimization function with appopriate arguments
+
+#T-test:
+#pchisq(D,df=1) where D - 2*difference of negative log likelihood
+
