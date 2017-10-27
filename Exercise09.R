f7e5c550ce3ed98670e090f6b081522e5442a352
Logan <larnold3@nd.edu>
2017-10-27 11:21:02 -0400

    Progress on number 2

--- Exercise09.R
+++ Exercise09.R
@@ -12,5 +12,47 @@
 # Minimization function with appopriate arguments
 
 #T-test:
-#pchisq(D,df=1) where D - 2*difference of negative log likelihood
+#pchisq(D,df=1) where D = 2*difference of negative log likelihood
+#Take output from both likelihood function to calculate p-value
 
+library(ggplot2)
+
+
+#### Problem 2 ####
+
+#Read in Data
+data2 <- read.csv(file = "MmarinumGrowth.csv", header = TRUE)
+attach(data2)
+df2 <- data.frame(x = S, y = u)
+
+#Graph of raw data
+graph2a <- ggplot(df2, aes(x = S, y = u)) + geom_point() + theme_classic()
+
+#Create liklikhood function
+###Same as in-class example (continuous, normal distribution)
+###BUT, no longer a linear model - change formula for expected
+nllike2 <- function(p, x, y){
+  umax = p[1]
+  Ks = p[2]
+  sigma = exp(p[3])
+  
+  expected = umax*S/(S + Ks)
+  
+  nll = -sum(dnorm(x = y, mean = expected, sd = sigma, log = TRUE))
+  return(nll)
+}
+
+#Estimate Umax, Ks, sigma
+initialGuess = c(1,1,1)
+fit2 = optim(par = initialGuess, fn=nllike2, x = S, y = u)
+
+#Creating data to test the estimated parameters
+testx <- seq(0, 1000, length.out = 100)
+testy <- fit$par[1]*testx/(testx+fit$par[2])
+df2b <- data.frame(testx,testy)
+
+#Graphing the estimated model
+graph2b <- ggplot(df2b, aes(x = testx, y = testy)) + geom_point() + theme_classic()
+
+D2 <- 2*fit2$value
+pchisq(D2,df=1)
