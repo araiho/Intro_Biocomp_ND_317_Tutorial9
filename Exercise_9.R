1b701dfedcd9ec91346dfbb4bda6d318bcc4f6ff
Libby Fortin <efortin@nd.edu>
2017-10-27 13:36:21 -0400

    Question 1 edited, Question 3 partially done

--- Exercise_9.R
+++ Exercise_9.R
@@ -1,3 +1,5 @@
+#Question 1
+
 data <- read.csv("/Users/elizabethfortin12/Documents/ND First Year/Biocomputing/R_Programming/Exercise_9/ponzr1.csv")
 
 data$WTorNOT <- 0
@@ -12,7 +14,7 @@ for (i in 1:nrow(data)){
 }
 
 #Null model
-nll1 <- function(p,x,y){
+nll1 <- function(p,y){
   B0=p[1]
   sig=exp(p[2])
   expected=B0
@@ -20,8 +22,8 @@ nll1 <- function(p,x,y){
   return(nll)
 }
 
-initialGuess <- c(1,1)
-fit <- optim(par=initialGuess,fn=nll1,x=data$WTorNOT,y=data$ponzr1Counts)
+initialGuess <- c(2000,1)
+fit <- optim(par=initialGuess,fn=nll1,y=data$ponzr1Counts)
 fit
 
 #Linear Model
@@ -41,5 +43,44 @@ fit2
 #likelihood ratio test
 D<-2*(fit2$value - fit$value)
 ratiotest <- pchisq(q=D, df=1, lower.tail=FALSE)
+ratiotest
 
 
+
+
+#Question 3
+
+decomp <- read.csv("/Users/elizabethfortin12/Documents/ND First Year/Biocomputing/R_Programming/Exercise_9/leafDecomp.csv")
+
+#Null model
+nllnull <- function(p,d){
+  a=p[1]
+  sig=exp(p[2])
+  expected=a
+  nll=-sum(dnorm(x=d, mean=expected, sd=sig,log=TRUE))
+  return(nll)
+}
+
+initialGuess <- c(500,1)
+null <- optim(par=initialGuess,fn=nllnull,d=decomp$decomp)
+null
+
+#Humpshaped model
+nllhump <- function(p,Ms,d){
+  a=p[1]
+  b=p[2]
+  c=p[3]
+  sig=exp(p[4])
+  expected=a+b*Ms+c*Ms^2
+  nll=-sum(dnorm(x=d, mean=expected, sd=sig,log=TRUE))
+  return(nll)
+}
+
+initialGuess <- c(1,1,1,1)
+hump <- optim(par=initialGuess,fn=nllhump,Ms=decomp$Ms,d=decomp$decomp)
+hump
+
+#likelihood ratio test
+E<-2*(hump$value-null$value)
+ratiotest2 <- pchisq(q=E, df=2, lower.tail=FALSE)
+ratiotest2
